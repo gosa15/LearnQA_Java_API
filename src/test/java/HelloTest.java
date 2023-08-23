f1b812f5b0b6c42de17a9a96b0f247cca8891f8a
Круглов Сергей <gosa15@mail.ru>
2023-08-23 15:54:20 +0400

    Ex4: GET-запрос

--- src/test/java/HelloTest.java
+++ src/test/java/HelloTest.java
@@ -1,10 +1,16 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 
 public class HelloTest {
 
    @Test
-    public void testHello(){
-       System.out.println("Hello from Kate");
+    public void testAPI(){
+       Response response = RestAssured
+               .get("https://playground.learnqa.ru/api/get_text")
+               .andReturn();
+
+       response.prettyPrint();
    }
 
 }
