5b5e11b574bd29b30b984c29ae6a7b9a849c7f51
Круглов Сергей <gosa15@mail.ru>
2023-08-24 18:20:53 +0400

    Ex6: Редирект

---
+++ src/test/java/EX6Redirect.java
@@ -0,0 +1,23 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+
+public class EX6Redirect {
+
+    @Test
+    public void testAPI(){
+        Response response = RestAssured
+                .given()
+                .redirects()
+                .follow(false)
+                .when()
+                .get("https://playground.learnqa.ru/api/long_redirect")
+                .andReturn();
+
+
+        String locationHeaders = response.getHeader("Location");
+        System.out.println(locationHeaders);
+    }
+
+}
